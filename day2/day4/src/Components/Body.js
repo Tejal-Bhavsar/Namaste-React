12f39234b67ec7647785e186c6784a65c63e549e
tejal-bhavsar <tejalbhavsar02@gmail.com>
2023-02-05 15:13:56 +0530

    lecture 5

--- day2/day4/src/Components/Body.js
+++ day2/day4/src/Components/Body.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import RestrontCard from './RestrontCard'
 import { restaurantList } from '../config'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Body() {
   const [search, setSearch ] = useState('')
   const [restaurant, setRestorant ] = useState(restaurantList)
+
+  useEffect(() => {
+    getRestaurents();
+   }, []);
+
+   //async
+
+    async function getRestaurents() {
+        //API Call
+      const data = await axios(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.6107101&lng=88.42706020000001&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json = await (data).json();
+      //console.log(json);
+      //optional chaining
+      // setAllRestaurents(json?.data?.cards[2]?.data?.data?.cards)
+      // setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+    }
 
   const filterRestorant = (search, restaurant) => {
      let data =  restaurant.filter((item) => search.toLowerCase() == item.data.name.toLowerCase())
